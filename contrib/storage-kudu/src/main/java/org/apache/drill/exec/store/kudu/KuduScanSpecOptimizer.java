c95478242b496fdb7bf5198e69a80fff226f0f3c
Przemyslaw Maciolek <pmm@collective-sense.com>
2016-11-11 01:19:46 +0100

    Rework Optimizer. Still needs to update how the results are presented (rebuilding KuduScanSpec?)

--- contrib/storage-kudu/src/main/java/org/apache/drill/exec/store/kudu/KuduScanSpecOptimizer.java
+++ contrib/storage-kudu/src/main/java/org/apache/drill/exec/store/kudu/KuduScanSpecOptimizer.java
@@ -22,9 +22,13 @@ import org.apache.kudu.Schema;
 import org.apache.kudu.client.KuduPredicate;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
+import java.util.TreeSet;
+
 
 public class KuduScanSpecOptimizer {
     private KuduScanSpec input;
@@ -61,6 +65,7 @@ public class KuduScanSpecOptimizer {
     /**
      * Rebuilds the scan spec, removing anything that could effect in inefficient OR query
      * (not having full path throguh primary key columns)
+     * @deprecated  NO, don't use it - clean this up
      */
     public KuduScanSpec pruneScanSpec(KuduScanSpec input) {
         // This is simplistic approach, which makes global view on the predicates and constraints
@@ -142,7 +147,9 @@ public class KuduScanSpecOptimizer {
         List<List<KuduPredicate>> inputPermutationSets = new ArrayList<>();
         inputPermutationSets.add(new ArrayList<KuduPredicate>());
 
-        return permutateScanSpec(inputPermutationSets, scanSpec);
+        List<List<KuduPredicate>> basePermutationSet = permutateScanSpec(inputPermutationSets, scanSpec);
+
+        return new NonPrimaryKeyPermutationPruner(basePermutationSet).pruneNonLinkedKeys();
     }
 
     private List<List<KuduPredicate>> permutateScanSpec(List<List<KuduPredicate>> inputPermutationSets, KuduScanSpec scanSpec) {
@@ -182,5 +189,113 @@ public class KuduScanSpecOptimizer {
         }
 
         return newLists;
+    }
+
+
+    /**
+     * We need to check if there are no alternative scans on non-priority key items
+     * In some cases they are good, but in some they bring too much trouble. The actual case
+     * depends on multiple factors and we keep it simple here, so use naive rule for pruning such
+     * cases. This also means that with more complex structures, it won't really work.
+     */
+    private class NonPrimaryKeyPermutationPruner {
+        private List<List<KuduPredicate>> permutationSet;
+
+        public final static int MAX_NON_PRIMARY_KEY_ALTERNATIVES = 1;
+
+        NonPrimaryKeyPermutationPruner(List<List<KuduPredicate>> permutationSet) {
+            this.permutationSet = permutationSet;
+        }
+
+        private Set<String> findGaps(List<KuduPredicate> predicates) {
+            Set<String> leftCols = new HashSet<String>();
+            for (KuduPredicate pred : predicates) {
+                leftCols.add(pred.toPB().getColumn());
+            }
+
+            for (String primaryKey : primaryKeys) {
+                if (leftCols.contains(primaryKey)) {
+                    // Good, we have it, so far it's valid, lets move to the next...
+                    leftCols.remove(primaryKey);
+                } else {
+                    // We have a gap - whatever is left should not be included on any path within an OR query
+                    return leftCols;
+                }
+            }
+
+            return leftCols;
+        }
+
+        private Set<KuduPredicate> findLinkedPrimaryKeyPart(List<KuduPredicate> permutation) {
+            Set<KuduPredicate> linkedPart = new HashSet<>();
+
+            Map<String, KuduPredicate> leftCols = new HashMap<>();
+            for (KuduPredicate pred : permutation) {
+                leftCols.put(pred.toPB().getColumn(), pred);
+            }
+
+            for (String primaryKey : primaryKeys) {
+                if (leftCols.keySet().contains(primaryKey)) {
+                    // Good, we have it, so far it's valid, lets move to the next...
+                    linkedPart.add(leftCols.remove(primaryKey));
+                } else {
+                    // We have a gap - whatever is left should not be included on any path within an OR query
+                    return linkedPart;
+                }
+            }
+
+            return linkedPart;
+        }
+
+        public List<List<KuduPredicate>> pruneNonLinkedKeys() {
+            List<List<KuduPredicate>> prunedPermutations = new ArrayList<>();
+
+            Map<Set<KuduPredicate>, List<Set<KuduPredicate>>> linkedPrimaryKeyPartToRest = new HashMap<>();
+
+            for (List<KuduPredicate> permutation : permutationSet) {
+                // If KuduPredicate would implement Comparable then TreeSet could have been better a choice
+                Set<KuduPredicate> linkedPrimaryKeyPredicates = new HashSet<>();
+                Set<KuduPredicate> nonLinkedPredicates = new HashSet<>();
+
+                linkedPrimaryKeyPredicates = findLinkedPrimaryKeyPart(permutation);
+                for (KuduPredicate pred : permutation) {
+                    if (!linkedPrimaryKeyPredicates.contains(pred)) {
+                        nonLinkedPredicates.add(pred);
+                    }
+                }
+
+                if (nonLinkedPredicates.isEmpty()) {
+                    // Just pass the current permutation - it's safe
+                    prunedPermutations.add(permutation);
+                } else {
+                    List<Set<KuduPredicate>> list = linkedPrimaryKeyPartToRest.get(linkedPrimaryKeyPredicates);
+
+                    if (list == null) {
+                        list = new ArrayList<>();
+                        linkedPrimaryKeyPartToRest.put(linkedPrimaryKeyPredicates, list);
+                    }
+                    list.add(nonLinkedPredicates);
+                }
+            }
+
+            // For any permutation of concern, see how many filters are there
+            for (Set<KuduPredicate> linkedKeyPart : linkedPrimaryKeyPartToRest.keySet()) {
+                List<Set<KuduPredicate>> nonLinkedKeySets = linkedPrimaryKeyPartToRest.get(linkedKeyPart);
+                if (nonLinkedKeySets.size() > MAX_NON_PRIMARY_KEY_ALTERNATIVES) {
+                    // Skip the permutations
+                    prunedPermutations.add(new ArrayList<>(linkedKeyPart));
+                } else {
+                    // Include the permutations
+                    for (Set<KuduPredicate> nonPrimaryKeyPredicates : nonLinkedKeySets) {
+                        List<KuduPredicate> permutation = new ArrayList<>(linkedKeyPart);
+                        permutation.addAll(nonPrimaryKeyPredicates);
+
+                        prunedPermutations.add(permutation);
+                    }
+                }
+            }
+
+            return prunedPermutations;
+        }
     }
 }

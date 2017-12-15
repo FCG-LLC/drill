633eb0e7b66a0f0377241113ca4fb9246be5c446
pawel@collective-sense.com <pafeu81@gmail.com>
2017-12-15 15:13:10 +0100

    Fixed idiotic drill checkstyle

--- contrib/storage-kudu/src/main/java/org/apache/drill/exec/store/kudu/NonPrimaryKeyPermutationPruner.java
+++ contrib/storage-kudu/src/main/java/org/apache/drill/exec/store/kudu/NonPrimaryKeyPermutationPruner.java
@@ -4,7 +4,16 @@ package org.apache.drill.exec.store.kudu;
 import org.apache.kudu.Common;
 import org.apache.kudu.client.KuduPredicate;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
 
 /**
  * We need to check if there are no alternative scans on non-priority key items

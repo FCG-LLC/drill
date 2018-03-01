2e892e70fe77df4f094863543a2697f4cadd10c2
Kildyt <kildyt@gmail.com>
2018-03-01 17:56:37 +0100

    Removed static fields from bitwise

--- customfuncs/bitwise/src/main/java/cs/drill/bitwise/NumberBits.java
+++ customfuncs/bitwise/src/main/java/cs/drill/bitwise/NumberBits.java
@@ -14,13 +14,13 @@ public final class NumberBits {
             nulls = FunctionTemplate.NullHandling.NULL_IF_NULL
     )
     public static class NumberBitsOfBigIntFunction implements DrillSimpleFunc {
-        private static final byte BITS_WIDTH = BigIntHolder.WIDTH * 8;
-
         @Param BigIntHolder number;
         @Output BaseWriter.ComplexWriter out;
 
         @Override
-        public void setup() {}
+        public void setup() {
+            System.out.println("SETUP !!!!!!!!");
+        }
 
         @Override
         public void eval() {
@@ -28,7 +28,7 @@ public final class NumberBits {
             BaseWriter.ListWriter list = out.rootAsList();
             list.startList();
             byte i = 0;
-            while (input != 0 && i < BITS_WIDTH) {
+            while (input != 0 && i < 64) {
                 if ((input & 1) > 0) {
                     list.tinyInt().writeTinyInt(i);
                 }
@@ -45,8 +45,6 @@ public final class NumberBits {
             nulls = FunctionTemplate.NullHandling.NULL_IF_NULL
     )
     public static class NumberBitsOfIntFunction implements DrillSimpleFunc {
-        private static final byte BITS_WIDTH = IntHolder.WIDTH * 8;
-
         @Param IntHolder number;
         @Output BaseWriter.ComplexWriter out;
 
@@ -59,7 +57,7 @@ public final class NumberBits {
             BaseWriter.ListWriter list = out.rootAsList();
             list.startList();
             byte i = 0;
-            while (input != 0 && i < BITS_WIDTH) {
+            while (input != 0 && i < 32) {
                 if ((input & 1) > 0) {
                     list.tinyInt().writeTinyInt(i);
                 }
@@ -76,8 +74,6 @@ public final class NumberBits {
             nulls = FunctionTemplate.NullHandling.NULL_IF_NULL
     )
     public static class NumberBitsOfSmallIntFunction implements DrillSimpleFunc {
-        private static final byte BITS_WIDTH = SmallIntHolder.WIDTH * 8;
-
         @Param SmallIntHolder number;
         @Output BaseWriter.ComplexWriter out;
 
@@ -90,7 +86,7 @@ public final class NumberBits {
             BaseWriter.ListWriter list = out.rootAsList();
             list.startList();
             byte i = 0;
-            while (input != 0 && i < BITS_WIDTH) {
+            while (input != 0 && i < 16) {
                 if ((input & 1) > 0) {
                     list.tinyInt().writeTinyInt(i);
                 }
@@ -107,8 +103,6 @@ public final class NumberBits {
             nulls = FunctionTemplate.NullHandling.NULL_IF_NULL
     )
     public static class NumberBitsOfTinyIntFunction implements DrillSimpleFunc {
-        private static final byte BITS_WIDTH = TinyIntHolder.WIDTH * 8;
-
         @Param TinyIntHolder number;
         @Output BaseWriter.ComplexWriter out;
 
@@ -121,7 +115,7 @@ public final class NumberBits {
             BaseWriter.ListWriter list = out.rootAsList();
             list.startList();
             byte i = 0;
-            while (input != 0 && i < BITS_WIDTH) {
+            while (input != 0 && i < 8) {
                 if ((input & 1) > 0) {
                     list.tinyInt().writeTinyInt(i);
                 }
